d8ab7a28d2e4d12c3dd30fe9c076892c0443231a
XYWang-prog <xw3016@columbia.edu>
2026-08-18 23:05:38 -0400

    Fix router basename for Vercel root deployment

--- src/main.tsx
+++ src/main.tsx
@@ -6,10 +6,8 @@ import './styles/index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter
-      basename="/nyc-rental-finder"
-      future={{ v7_relativeSplatPath: true, v7_startTransition: true }}
-    >
+    {/* 不再设置 basename：Vercel 部署在根路径（原来的 GitHub Pages 需要 /nyc-rental-finder 前缀） */}
+    <BrowserRouter future={{ v7_relativeSplatPath: true, v7_startTransition: true }}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
